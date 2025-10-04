699b55e6bdedae69454fb207183623a844e0b515
vitlyc <nicebackflip@gmail.com>
2025-10-04 18:59:31 +0300

    client: refac documents link in tab

---
+++ client/src/components/CaseForms/DocumentsForm.jsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { Box, Typography, Link } from "@mui/material"
+import FolderIcon from "@mui/icons-material/Folder"
+
+export default function DocumentsForm({ form, onChange }) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", gap: 2, mt: 1 }} pl={1.6}>
+      <FolderIcon />
+      <Link
+        href={form?.documents}
+        target="_blank"
+        rel="noopener noreferrer"
+        sx={{ textDecoration: "none", color: "inherit" }} // Убираем подчеркивание и устанавливаем цвет как у обычного текста
+      >
+        <Typography>
+          {form?.defendants[0].surname} {form?.defendants[0].name}{" "}
+          {form?.defendants[0].patronymic}
+        </Typography>
+      </Link>
+    </Box>
+  )
+}
